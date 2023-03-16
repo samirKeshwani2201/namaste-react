269b07a2e2e461a05961662d161cd6b0ea64cafc
Samir Keshwani <samirkeshwani07@gmail.com>
2023-03-16 15:07:26 +0530

    video 8 Exploring the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { restaurantList } from "../constants"
 import RestrauntCard from "./RestrurantCard";
+import Shimmer from "./Shimmer"
 
-
-function filterData(searchText, restaurants) {
-    return restaurantList.filter((restaurant) => restaurant.data.name.includes(searchText))
+function filterData(searchText, allrestaurants) {
+    return allrestaurants.filter((restaurant) => restaurant?.data?.name?.toLowerCase()?.includes(searchText?.toLowerCase()))
 
 }
+
+
 const Body = () => {
-    const [restaurants, setRestaurants] = useState(restaurantList);
+    const [filteredrestaurants, setFilteredRestaurants] = useState([]);
+    const [allrestaurants, setAllRestaurants] = useState([]);
 
-    const [searchText, setSearchText] = useState();//used to create state variable
+    const [searchText, setSearchText] = useState("");//used to create state variable
 
-    return (
+    useEffect(() => {
+        getRestaurants();
+    }, []);
 
+
+    async function getRestaurants() {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385044&lng=78.486671&page_type=DESKTOP_WEB_LISTING");
+        // It returns a readable stream ,and it needs to be converted to json object 
+        const json = await data.json();
+        setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+        setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+    }
+
+    // Early return (not render component)
+    if (!allrestaurants)
+        return null;
+
+    if (allrestaurants.length!==0 && filteredrestaurants?.length === 0) return <h1>No restraunt match your search</h1>
+
+    return ( allrestaurants?.length === 0) ? (<Shimmer />) : (
         <>
             <div className="search-container">
                 <input type="text" className="search-input" placeholder="Search" value={searchText}
@@ -26,16 +47,16 @@ const Body = () => {
                     onClick={
                         () => {
                             // nedd to filter the data 
-                            const data = filterData(searchText, restaurants);
-                            setRestaurants(data);
+                            const data = filterData(searchText, allrestaurants);
+                            setFilteredRestaurants(data);
                         }
                     }
                 >Search</button>
             </div>
+
             <div className="restaurant-list">
                 {
-                    restaurants.map(restraurant => {
-
+                    filteredrestaurants.map(restraurant => {
                         return <RestrauntCard {...restraurant.data} key={restraurant.data.id} />
                     })
                 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,39 @@
+import { useState } from "react";
+
+const loggedInUser = () => {
+    // API call to check authentication 
+    return false;
+
+};
+
+
 const Title = () => (
     <a href="/">
         <img className="logo" alt="logo" src="https://yt3.googleusercontent.com/ytc/AL5GRJXudT76175T4x4n7eslWM1YkgNLHDSSqfXGoadl=s900-c-k-c0x00ffffff-no-rj" />
     </a>
 );
 
-const Header = () => (
-    <div className="header" >
-        <Title />
-        <div className="nav-items" >
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact </li>
-                <li>Cart</li>
-            </ul>
+const Header = () => {
+    const [isLoggedIn,setIsLoggedIn]=useState(false);
+
+    return (
+        <div className="header" >
+            <Title />
+            <div className="nav-items" >
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact </li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+
+            {
+                (isLoggedIn) ? (<button onClick={()=>setIsLoggedIn(false)}>Logout</button>) :( <button  onClick={()=>setIsLoggedIn(true)}>Login</button>)
+            }
+
         </div>
-    </div>
-);
+    );
+};
 
 export default Header;
